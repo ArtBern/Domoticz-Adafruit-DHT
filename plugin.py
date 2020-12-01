f2e8daa8867dd579f6d216ae1aedf5c1d7622a34
Бернадский Артем Павлович <apbernad@mts.ru>
2020-12-01 18:04:33 +0300

    draft realization

--- plugin.py
+++ plugin.py
@@ -1,9 +1,9 @@
-#		   CCS811 Plugin
+#		   Adafruit DHT Plugin
 """
-<plugin key="CCS811Reader" name="CO2 CCS811 Reader" author="artbern" version="1.0.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://www.google.com/">
+<plugin key="AdafruitDHT" name="Adafruit DHT Sensors Reader" author="artbern" version="1.0.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://www.google.com/">
 	<description>
-		<h2>CCS811 Reader</h2><br/>
-		Overview...
+		<h2>Adafruit DHT Sensors Reader</h2><br/>
+		Plugin for reading onboard DHT sensors (DHT11, DHT22, AM2301)
 		<h3>Features</h3>
 		<ul style="list-style-type:square">
 			<li>Feature one...</li>
@@ -17,10 +17,11 @@
 		Configuration options...
 	</description>
 	<params>
-		<param field="Mode6" label="Debug" width="75px">
+		<param field="Mode1" label="GPIO Port" width="30px"  required="true" default="22"/>
+		<param field="Mode2" label="DHT Type" width="30px"  required="true"/>
 			<options>
-				<option label="True" value="Debug"/>
-				<option label="False" value="Normal"  default="true" />
+				<option label="DHT11" value="11"/>
+				<option label="DHT22/AM2302" value="22"  default="true" />
 			</options>
 		</param>    
 	</params>
@@ -32,8 +33,7 @@ class BasePlugin:
 	enabled = False
 	
 	def __init__(self):
-		self.ccs811 = None
-		self.lastGoodMeasured = dict(temp=20,humi=30,co2=800)
+		self.lastGoodMeasured = dict(temp=20,humi=30)
 		
 		
 
@@ -45,28 +45,17 @@ class BasePlugin:
 			Domoticz.Debugging(1)
 
 		if (len(Devices) == 0):
-			Domoticz.Device(Name="CCS811 C02", Unit=1, TypeName="Air Quality", Used=1).Create()
+			Domoticz.Device(Name="Adafruit DHT", Unit=1, TypeName="Temp+Hum", Used=1).Create()
 
 		Domoticz.Debug("Device created.")
 		DumpConfigToLog()
 
 		try:
 			#installed with 
-			#sudo pip3 install adafruit-circuitpython-ccs811
-			import board
-			import busio
-			import adafruit_ccs811
-			 
-			i2c = busio.I2C(board.SCL, board.SDA)
-			ccs811 = adafruit_ccs811.CCS811(i2c)        
-			
-			# Wait for the sensor to be ready and calibrate the thermistor
-			i = 0
-			while not ccs811.data_ready and i < 10000:
-				i = i + 1
-				pass
-			
-			self.ccs811 = ccs811
+            #sudo pip3 install Adafruit_DHT
+            #sudo apt-get install libgpiod2
+            
+			import Adafruit_DHT
 			
 		except ImportError as e:
 			Domoticz.Log("Error loading Adafruit_CCS811: {0}:{1}".format(e.__class__.__name__, e.message)) 
@@ -99,21 +88,22 @@ class BasePlugin:
 		try:
 
 			Domoticz.Debug("In onHeartBeat. Prev value:       '" + str(self.lastGoodMeasured["co2"]) + "'")
+
+            rawHumi, rawTemp = Adafruit_DHT.read_retry(int(Parameters["Mode2"]), int(Parameters["Mode1"]))
 		
-			if self.ccs811 is not None:
-				self.lastGoodMeasured = dict(temp=self.ccs811.temperature,humi=40,co2=self.ccs811.eco2)
+			self.lastGoodMeasured = dict(temp=rawTemp,humi=rawHumi)
 
 			#if (rawHumi is not None and rawHumi < 101) and rawTemp is not None:
 			#   self.temperatureBuffer.append(rawTemp)
 			#print "@%s, Average: %s" % (self.temperatureBuffer, self.temperatureBuffer.average)
-			 #  self.lastGoodMeasured = dict(temp=self.temperatureBuffer.average,humi=rawHumi,co2=800)
-
+			#  self.lastGoodMeasured = dict(temp=self.temperatureBuffer.average,humi=rawHumi,co2=800)
+            
 		except OSError as e:
 			Domoticz.Log("OSError reading Adafruit_CCS811: {0}:{1}".format(e.__class__.__name__, str(e)))
 		except RuntimeError as e:
 			Domoticz.Log("RuntimeError reading Adafruit_CCS811: {0}:{1}".format(e.__class__.__name__, str(e)))
 
-		UpdateDevice(1,self.lastGoodMeasured["co2"],'',100)
+        UpdateDevice(1, 0,  + str(rawTemp) + ";" + str(rawHumi), 100)
 		
 def UpdateDevice(Unit, nValue, sValue, batterylevel):
 	# Make sure that the Domoticz device still exists (they can be deleted) before updating it 
